9bbc7fa0cd8dcbab30fbf043f742580d274e4c43
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-06 23:16:01 +0500

    add model

---
+++ Homework2/Models/Character.swift
@@ -0,0 +1,29 @@
+//
+//  DisneyCharacters.swift
+//  Homework2
+//
+//  Created by Юлия Ястребова on 06.05.2023.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    let name: String
+    let culture: String
+    let born: String
+    let died: String
+    let titles: [String]
+    let aliases: [String]
+    let allegiances: [String]
+}
+
+struct House: Decodable {
+    let url: String
+    let name: String
+    let region: String
+    let coatOfArms: String
+    let words: String
+}
+
+
+
