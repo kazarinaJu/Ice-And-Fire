802658d1e011648eb413c4c649b0ef4c5c19caa5
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-05-09 23:16:38 +0500

    add param in model

--- IceAndFire/Models/Character.swift
+++ IceAndFire/Models/Character.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct Character: Decodable {
+    let url: String
     let name: String
     let culture: String
     let born: String
@@ -21,6 +22,10 @@ struct House: Decodable {
     let url: String
     let name: String
     let region: String
+    let coatOfArms: String
+    let currentLord: String
+    let heir: String
+    let overlord: String
     let words: String
     let swornMembers: [String]
 }
